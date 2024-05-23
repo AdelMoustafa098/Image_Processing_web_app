f96ed3a4b0a8c23d2d56613f91819e09d2a4896c
AdelMoustafa098 <adel.moussa939@gmail.com>
2024-05-23 23:21:55 +0300

    end to end test

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import random
-import numpy as np
 import cv2
-from image_processer import ImageProcessor
-
+from image_processor import ImageProcessor
 
 
 img = cv2.imread("Test Images/2_50.jpg", cv2.IMREAD_COLOR)
 
 
 ip = ImageProcessor(img)
-ip.convert_to_gry()
-ip.reset_image()
+# ip.convert_to_gry()
+# ip.reset_image()
+ip.add_salt_pepper_noise()
 x = ip.get_image()
 cv2.imshow("image", x)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
