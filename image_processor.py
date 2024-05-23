18ba4770858a98dc095a804bd7582c84694e1571
AdelMoustafa098 <adel.moussa939@gmail.com>
2024-05-23 23:20:47 +0300

    editing s&p filter to be faster

--- image_processor.py
+++ image_processor.py
@@ -1,69 +1,84 @@
 import numpy as np
 
 
+class ImageProcessor:
+    """
+    This class is used for image processing
+    """
 
-
-class ImageProcessor():
     def __init__(self, image):
         self.image = image
         self.orignal_image = image
+        self.gray_image = False
 
     def get_image(self):
         """
-            This method returns the image
+        This method returns the image
 
-            Arguments: None
-            Returns: image (numpay array)
+        Arguments: None
+        Returns: image (numpay array)
         """
         return self.image
-    
-    
+
     def reset_image(self):
         """
-            This method resets the image to the original input image removing any processing done on it
+        This method resets the image to the original input image removing any processing done on it
 
-            Arguments: None
-            Returns: None
+        Arguments: None
+        Returns: None
         """
         self.image = self.orignal_image
 
+    def is_gray(self):
+        """
+        This method checks if the image is gray scale.
+
+        Arguments: None
+        Returns: bool
+        """
+        return len(self.image.shape) == 2
 
     def convert_to_gry(self):
         """
-            This method convert an RGB image to gray scale image doing the follwoing:
+        This method convert an RGB image to gray scale image doing the follwoing:
 
-                1- extract the rgb channels from the image.
-                2- multiply each channel by a constant and sum the result.
-                3- convert the result from step 2 to a sutible data type.
+            1- extract the rgb channels from the image.
+            2- multiply each channel by a constant and sum the result.
+            3- convert the result from step 2 to a sutible data type.
 
-            Arguments: None
-            Returns: None
+        Arguments: None
+        Returns: None
 
         """
-        
-        b, g, r = self.image[:,:,0], self.image[:,:,1], self.image[:,:,2]
+
+        b, g, r = self.image[:, :, 0], self.image[:, :, 1], self.image[:, :, 2]
         gray = 0.2989 * r + 0.5870 * g + 0.1140 * b
         self.image = gray.astype(np.uint8)
 
-
     def add_salt_pepper_noise(self):
         """
-            This method add salt and pepper nois to the image
-            
-            Arguments: None
-            Returns: None
+        This method add salt and pepper noise to the gray image
+
+        Arguments: None
+        Returns: None
         """
+        # make sure image is gray
+        if not self.is_gray():
+            self.convert_to_gry()
+
         row, col = self.image.shape
-        selected_pixel=random.randint(100,5000)
+        selected_pixel = np.random.randint(100, 5000)
 
-        for i in range(selected_pixel):
-            # set these pixel to white
-            x_white=random.randint(0,col-1)
-            y_white=random.randint(0,row-1)
-            self.image[y_white][x_white]=255
+        # Generate coordinates for white (salt) noise
+        white_coords = (
+            np.random.randint(0, row, selected_pixel),
+            np.random.randint(0, col, selected_pixel),
+        )
+        self.image[white_coords] = 255
 
-        for i in range(selected_pixel):
-            # set these pixel to black
-            x_black=random.randint(0,col-1)
-            y_black=random.randint(0,row-1)
-            self.image[y_black][x_black]=0
+        # Generate coordinates for black (pepper) noise
+        black_coords = (
+            np.random.randint(0, row, selected_pixel),
+            np.random.randint(0, col, selected_pixel),
+        )
+        self.image[black_coords] = 0
